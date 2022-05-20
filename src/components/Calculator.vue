06ba5f9504afa0b4646e09386759cd316ede8f66
mmd-ghodoosi <mamadakgh@gmail.com>
2022-05-20 16:54:03 +0430

    buttons

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,10 +2,24 @@
   <div class="p-3" style="max-width:400px; margin: 50px auto; background-color: #234 ">
    
    <!---calculator result-->
-    
+
     <div class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark">
-      0
+      {{ calculatorValue || 0   }}
     </div>
+
+    <!---calculator buttons-->
+
+  <div class="row no-gutters" >
+     <div class="col-3" v-for="n in calculatorElements" :key="n">
+        <div class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class"
+        :class="{'bg-vue-green': ['C','*','/','-','+','%','='].includes(n)}"
+        @click="action(n)"
+        >
+        {{ n }}
+        </div>
+     </div>
+  </div>
+
 
   </div>
 </template>
@@ -19,13 +33,30 @@ export default {
   data(){
  return{
    calculatorValue:'',
+   calculatorElements: ['C','*','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
  }
-  }
+  },
+
+   methods:{
+     action(n){
+       if(!isNaN(n) || n === '.'){
+         this.calculatorValue += n + '';
+       }
+     }
+   }
+
 };
 </script>
 
 <style scoped>
 .bg-vue-dark{
   background: #31475e;
+}
+.hover-class:hover{
+  cursor: pointer;
+  background: #3D5875;
+}
+.bg-vue-green{
+  background: #3fb884;
 }
 </style>
